f44d8ff671575dc6b7d382157e9bfee9092e410a
hemalatha chintalapally <hemalatha.chintalapally@gmail.com>
2020-05-23 23:32:49 -0500

    added input validation

--- ielts-score-calculator.js
+++ ielts-score-calculator.js
@@ -1,6 +1,7 @@
 function ieltsCalculator()
 {
     var speaking = parseInt(document.getElementById("speakingtextbox").value);
+    console.log(speaking);
     var writing = parseInt(document.getElementById("writingtextbox").value);
     var listening = parseInt(document.getElementById("listeningtextbox").value);
     var reading = parseInt(document.getElementById("readingtextbox").value);
@@ -12,7 +13,24 @@ function ieltsCalculator()
     }
     else
     {
-        document.getElementById("displaymessage").innerHTML = "Please enter valid input";
+       // document.getElementById("displaymessage").innerHTML = "Please enter valid input";
+        if(isNaN(speaking))
+        {
+            document.getElementById("displaymessage").innerHTML = "Enter valid numeric value for speaking";
+        }
+        else if(isNaN(listening))
+        {
+            document.getElementById("displaymessage").innerHTML = "Enter valid numeric value for listening";
+        }
+        else if(isNaN(reading))
+        {
+            document.getElementById("displaymessage").innerHTML = "Enter valid numeric value for reading";
+        }
+        else if(isNaN(writing))
+        {
+            document.getElementById("displaymessage").innerHTML = "Enter valid numeric value for writing";
+        }
+
     }
 }
 function clearValues()
